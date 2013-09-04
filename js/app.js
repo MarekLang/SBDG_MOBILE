c50205e857932bbacd7da151c183dd69ed4b5e31
Marek Lang <marek.hubek@gmail.com>
2013-09-04 13:06:21 +0200

    Test 005

--- js/app.js
+++ js/app.js
@@ -16,4 +16,23 @@ $(document).on( "mobileinit", function() {
     $.mobile.buttonMarkup.hoverDelay = 0;
     $.mobile.pushStateEnabled = false;
     $.mobile.defaultPageTransition = "none";
+	loadRepos();
 });
+
+
+
+function loadRepos() {
+	$.ajax({
+	url: 'http://www.sebadiagnoza.sk/Services/data.svc/GetData',
+	type: 'POST'
+	}).done(function(data) {
+	//$("#indexPage #content #flexDiv").text(data.Charts[1]["TENG"]);
+		var i, repo;
+		$.each(data.Charts, function (i, repo) {
+			$("#Charts").append("<li><a href='repo-detail.html?owner=" + repo["C"] + "'>"
+			+ "<h4>" + repo["TENG"] + "</h4>"
+			+ "<p>" + repo["STENG"] + "</p></a></li>");
+		});
+		$('#Charts').listview('refresh');
+	});
+}
